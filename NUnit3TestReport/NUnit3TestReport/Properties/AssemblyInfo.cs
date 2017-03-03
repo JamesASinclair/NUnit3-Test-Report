f0d628ec5ec7a3246bf0fe94d6c5ab7b45197dc1
JamesSinclairBiomni <james.sinclair@biomni.com>
2017-03-03 16:34:21 +0000

    Initial commit of empty solution

---
+++ NUnit3TestReport/NUnit3TestReport/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("NUnit3TestReport")]
+[assembly: AssemblyDescription("Processes a set of NUnit3 Xml files and makes an Html summary report file.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NUnit3 Test Report")]
+[assembly: AssemblyCopyright("Copyright © James Sinclair & Mat Roberts 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.*")]
